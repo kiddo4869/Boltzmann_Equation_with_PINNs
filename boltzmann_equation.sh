f3352f854fd941de4b822707b16d97ba9b376485
kiddo4869 <wanchikit227@gmail.com>
2024-08-08 22:14:12 +0800

    logging

--- boltzmann_equation.sh
+++ boltzmann_equation.sh
@@ -1,10 +1,10 @@
 #! bash
 
 # Debugging
-#python boltzmann_equation.py --debug --phase test
+python boltzmann_equation.py --debug --phase train
 
 # Adam Optimizer
-python boltzmann_equation.py --name adam --phase test --optimizer adam --log_loss
+#python boltzmann_equation.py --name adam --phase test --optimizer adam --log_loss
 
 # L-BFGS Optimizer
 #python boltzmann_equation.py --name l-bfgs --phase test --optimizer l-bfgs --log_loss
