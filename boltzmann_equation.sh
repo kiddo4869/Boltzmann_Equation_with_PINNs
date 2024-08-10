bb3a7a6574100b5163e744bf0c84a02fd6a4cb00
kiddo4869 <wanchikit227@gmail.com>
2024-08-10 14:12:35 +0800

    feature_added

--- boltzmann_equation.sh
+++ boltzmann_equation.sh
@@ -1,10 +1,10 @@
 #! bash
 
 # Debugging
-python boltzmann_equation.py --debug --phase train
+#python boltzmann_equation.py --name debug --debug --phase test #--dynamic_scaling # --fermi_scaling
 
 # Adam Optimizer
 #python boltzmann_equation.py --name adam --phase test --optimizer adam --log_loss
 
 # L-BFGS Optimizer
-#python boltzmann_equation.py --name l-bfgs --phase test --optimizer l-bfgs --log_loss
+python boltzmann_equation.py --name l-bfgs --phase test --optimizer l-bfgs --log_loss --dynamic_scaling
